8e2d95a81ee8059d0945ce3424f964c293938d68
turtttle <gill.glenn@rocketmail.com>
2021-08-04 00:25:33 -0400

    convert to new inputs

--- Hanbaiki/Assets/Scripts/Common/InputManager.cs
+++ Hanbaiki/Assets/Scripts/Common/InputManager.cs
@@ -1,42 +1,116 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 namespace Assets.Scripts
 {
     public enum ButtonEnum
     {
-        Jump,
-        Fire1,
-        Fire2
+        A,
+        B,
+        X,
+        Y,
+        Start
     }
 
     public static class InputManager
     {
-        private static T ReadInput<T>(List<InputControl<T>> inputs) where T : struct
+        private static float ReadInput(List<ButtonControl> inputs)
         {
             foreach (var input in inputs)
             {
-                if (input)
+                if (input == null) continue;
+
+                float val = input.ReadValue();
+                if (val != 0)
+                {
+                    return val;
+                }
+            }
+
+            return 0f;
+        }
+
+        private static bool ReadTrigger(List<ButtonControl> inputs)
+        {
+            foreach (var input in inputs)
+            {
+                if (input == null) continue;
+
+                bool val = input.wasPressedThisFrame;
+                if (val)
+                {
+                    return val;
+                }
+            }
+
+            return false;
+        }
+
+        private static List<ButtonControl> GetButtons(ButtonEnum id)
+        {
+            switch (id)
+            {
+                default:
+                case ButtonEnum.A:
+                    return new List<ButtonControl>() { 
+                        Gamepad.current?.aButton,
+                        Keyboard.current?.enterKey
+                    };
+                case ButtonEnum.B:
+                    return new List<ButtonControl>() {
+                        Gamepad.current?.bButton,
+                        Keyboard.current?.backspaceKey
+                    };
+                case ButtonEnum.X:
+                    return new List<ButtonControl>() {
+                        Gamepad.current?.xButton,
+                        Keyboard.current?.eKey
+                    };
+                case ButtonEnum.Y:
+                    return new List<ButtonControl>() {
+                        Gamepad.current?.yButton,
+                        Keyboard.current?.qKey
+                    };
+                case ButtonEnum.Start:
+                    return new List<ButtonControl>() { 
+                        Gamepad.current?.startButton,
+                        Keyboard.current?.escapeKey
+                    };
             }
         }
 
+
         public static float GetHorzAxis()
         {
-            if (Gamepad.current != null)
-            {
-                if (Gamepad.current.leftStick.ReadValue().x != 0)
-            }
-            else
-            {
-                return Keyboard.current;
-            }
-            return Mathf.Round(Input.GetAxisRaw("Horizontal"));
+            float left = ReadInput(new List<ButtonControl>() {
+                Gamepad.current?.leftStick?.left,
+                Keyboard.current?.aKey
+            });
+
+            float right = ReadInput(new List<ButtonControl>() { 
+                Gamepad.current?.leftStick?.right,
+                Keyboard.current?.dKey
+            });
+
+            return right - left;
         }
 
         public static float GetVertAxis()
         {
-            return Mathf.Round(Input.GetAxisRaw("Vertical"));
+            float up = ReadInput(new List<ButtonControl>() {
+                Gamepad.current?.leftStick?.up,
+                Keyboard.current?.wKey
+            });
+
+            float down = ReadInput(new List<ButtonControl>()
+            {
+                Gamepad.current?.leftStick?.down,
+                Keyboard.current?.sKey
+            });
+
+            return up - down;
         }
 
         public static Vector2 GetMovement()
@@ -44,14 +118,14 @@ namespace Assets.Scripts
             return new Vector2(GetHorzAxis(), GetVertAxis());
         }
 
-        public static bool GetButtonHeld(ButtonEnum button)
+        public static float GetButtonHeld(ButtonEnum button)
         {
-            return Input.GetButton(button.ToString());
+            return ReadInput(GetButtons(button));
         }
 
         public static bool GetButtonTrigger(ButtonEnum button)
         {
-            return Input.GetButtonDown(button.ToString());
+            return ReadTrigger(GetButtons(button));
         }
     }
 }
