a9caa493b105a51b90f69a368c343d220247624c
turtttle <gjgilles@uwaterloo.ca>
2021-09-14 10:30:35 -0400

    mock out some more journal pages

---
+++ Hanbaiki/Assets/Scripts/Controllers/Journal/Characters/CharacterEntryController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Controllers
+{
+    public class CharacterEntryController : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
