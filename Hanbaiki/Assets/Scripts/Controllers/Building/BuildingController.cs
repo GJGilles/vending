a5c95ea7bc999e11321691a0d76a7e6278dac78f
turtttle <gjgilles@uwaterloo.ca>
2021-09-25 16:36:07 -0400

    inside/outside working better

---
+++ Hanbaiki/Assets/Scripts/Controllers/Building/BuildingController.cs
@@ -0,0 +1,42 @@
+﻿using PotatoTools;
+using PotatoTools.Character;
+using UnityEngine;
+
+namespace Assets.Scripts.Controllers
+{
+    public class BuildingController : MonoBehaviour
+    {
+        public GameObject inside;
+        public GameObject outside;
+
+        public int maskLayer;
+        private SpriteRenderer mask;
+
+        public void Enter()
+        {
+            inside.SetActive(true);
+            outside.SetActive(false);
+
+            if (mask == null)
+            {
+                mask = new GameObject().AddComponent<SpriteRenderer>();
+                mask.transform.SetParent(Camera.main.transform);
+                mask.transform.localPosition = new Vector2(0, 0);
+                mask.sprite = Sprite.Create(new Texture2D(640, 360), new Rect(0, 0, 640, 360), new Vector2(0.5f, 0.5f), 32);
+                mask.color = Color.black;
+                mask.sortingLayerID = maskLayer;
+            }
+        }
+
+        public void Exit()
+        {
+            inside.SetActive(false);
+            outside.SetActive(true);
+
+            if (mask != null)
+            {
+                Destroy(mask.gameObject);
+            }
+        }
+    }
+}
---
+++ Hanbaiki/Assets/Scripts/Controllers/Building/DoorController.cs
@@ -0,0 +1,24 @@
+﻿using PotatoTools;
+using PotatoTools.Character;
+using UnityEngine;
+
+namespace Assets.Scripts.Controllers
+{
+    public class DoorController : SelectableController
+    {
+        public BuildingController building;
+        public bool inside = true;
+
+        public override void Select()
+        {
+            if (inside)
+            {
+                building.Exit();
+            }
+            else
+            {
+                building.Enter();
+            }
+        }
+    }
+}
